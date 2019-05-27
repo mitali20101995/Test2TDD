dd5b9b9df071dfa4182c727d8a646979ff7e0b4a
Mitali Ahir <mitaliahir3@gmail.com>
2019-05-27 10:55:18 -0400

    R1 Green

--- TTC_TDD/src/TTC.java
+++ TTC_TDD/src/TTC.java
@@ -3,7 +3,16 @@ public class TTC {
 	
 	public double calculateTotal(String[] from, String[] to)
 	{
-		return 0.0;
+		String f = from[0];
+		String t = to[0];
+		double totalCost = 0.0;
+		if(f == "Leslie" && t == "Don Mills")
+		{
+			totalCost = 2.50;
+		}
+		
+		
+		return totalCost;
 	}
 
 }
